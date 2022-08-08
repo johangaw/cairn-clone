6dd16e746c55d9af57a6a6ed6e6bbd8175ef4641
Johan Gawell <johan.gawell@svt.se>
2022-08-08 13:33:13 +0200

    Add CairnBoard composable

---
+++ app/src/main/java/com/example/cairnclone/ui/CairnBoard.kt
@@ -0,0 +1,135 @@
+package com.example.cairnclone.ui
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cairnclone.R
+import com.example.cairnclone.game.*
+
+@Composable
+fun CairnBoard(
+    state: BoardState,
+    performMove: (shamanId: ShamanId, newPos: Pos) -> Boolean,
+    performSpawn: () -> Boolean,
+) {
+    var selectedShaman by remember { mutableStateOf<Shaman?>(null) }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Village(Team.Forest)
+        Column {
+            repeat(state.board.height) { y ->
+                Row {
+                    repeat(state.board.width) { x ->
+                        val pos = Pos(x, y)
+                        val shaman = state.shamanAt(pos)
+                        BoardPiece(
+                            onClick = {
+                                val currentSelectedShaman = selectedShaman
+                                val adjacentSelectedShaman =
+                                    currentSelectedShaman?.pos?.adjacentDirection(
+                                        pos
+                                    ) != null
+                                val isSpawnTile = state.spawnActionTile.positions.contains(pos)
+                                when {
+                                    shaman != null -> selectedShaman = if(selectedShaman == shaman) null else shaman
+                                    currentSelectedShaman != null && adjacentSelectedShaman -> performMove(currentSelectedShaman.id, pos)
+                                    currentSelectedShaman == null && isSpawnTile -> performSpawn()
+                                    else -> {}
+                                }
+                            }
+                        ) {
+                            shaman?.let { ShamanPiece(shaman, selected = selectedShaman == shaman) }
+                        }
+                    }
+                }
+            }
+        }
+        Village(Team.Sea)
+    }
+}
+
+@Composable
+fun Village(team: Team) {
+    Box(
+        Modifier
+            .height(75.dp)
+            .fillMaxWidth()
+            .padding(4.dp)
+            .background(
+                if (team == Team.Sea) Color.Blue else Color.Green
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Village", color = Color.White, style = MaterialTheme.typography.h3)
+    }
+}
+
+@Composable
+fun BoardPiece(
+    onClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Box(
+        Modifier
+            .size(75.dp)
+            .padding(4.dp)
+            .background(Color.LightGray)
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center,
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun ShamanPiece(
+    shaman: Shaman,
+    modifier: Modifier = Modifier,
+    selected: Boolean = false,
+) {
+    val imageId = when (shaman.team) {
+        Team.Forest -> R.drawable.forest_shaman
+        Team.Sea -> R.drawable.sea_shaman
+    }
+    Image(
+        painterResource(id = imageId),
+        contentDescription = "",
+        modifier = modifier.graphicsLayer {
+            if (selected) {
+                scaleX = 1.3f
+                scaleY = 1.3f
+            }
+        })
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CairnBoardPreview() {
+    val state = remember {
+        BoardState(
+            Team.Sea,
+            Board(),
+            SpawnActionTile.SpawnWhite,
+            MoveActionTile.Orthogonally,
+            listOf(),
+            listOf(
+                Shaman(team = Team.Sea, pos = Pos(0, 0)),
+                Shaman(team = Team.Forest, pos = Pos(4, 4))
+            )
+        )
+    }
+    CairnBoard(state, { id, pos -> Log.d("Preview", "performMove"); false }, { Log.d("Preview", "performSpawn"); false })
+}
